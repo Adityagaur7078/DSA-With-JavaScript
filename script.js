527b5d1c7652237f41b687fcad7072f88637652a
Adityagaur7078 <adityagaur7078@gmail.com>
2026-02-27 23:59:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Sum of Two Number
+
+// let a = 9;
+// let l = 23;
+
+// console.log(a + l);
+
+
+// Normal Sum
+let a = 1;
+let b = 1;
+
+console.log(a + b);
+
+// String Sum
+let j = "2";
+let k = "2";
+
+console.log(j + k);
+
